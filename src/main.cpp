d06abcc29f91c5275da5592d275a646c2fc6ad45
jmnc2 <49760581+jmnc2@users.noreply.github.com>
2023-12-03 18:15:34 +0000

    Adding OLED display

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "Arduino.h"
+
+// for OLED display
+#include <Adafruit_SH110X.h>
+
+#define i2c_Address 0x3c  // initialize with the I2C addr 0x3C Typically eBay OLED's
+#define OLED_RESET -1     //   QT-PY / XIAO
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 13
@@ -33,6 +54,24 @@ word mapVoltToPSI(word p) {
     if (p > maxValue) { p = maxValue; }
     if (p < minValue) { p = minValue; }
     return map(p, minValue, maxValue, 0, sensorMaxPSI);
+}
+
+#define PSI_TO_BAR_CONST_shiftL24 17351225    //0.0689475729*2^24=17 351 224.85
+
+/**
+ * @brief Takes the pressure in PSI and returns it in bar with the 4 digits after the decimal point in r.
+ * 
+ * @param p The pressure in PSI
+ * @param r the remainder x 10000. i.e. 4 places after the point, 0.xxxx
+ * @return word the whole number part of the bars
+ */
+word PSItoBar(word p, word &r){
+    uint32_t bar;
+    bar = PSI_TO_BAR_CONST_shiftL24 * (uint32_t)p;
+    bar = bar >> 16;
+    r = bar bitand 0xFF;
+    r = ((uint32_t)r *10000) >> 8;
+    return (bar >> 8);
 }
 
 void pumpOff() {
@@ -102,6 +141,22 @@ void ledBlink(byte state = 0) {
         delay(2000);
     }
 }
+
+byte oledWriteAt(byte n, byte x_char, byte y_line, byte padTo = 0, byte textSize = 1, byte charWidth = 6, byte charHight = 9) {
+    if (x_char < 1 or y_line < 1) return -1;
+    if (padTo > 0) {
+        display.fillRect((x_char - 1) * textSize * charWidth, (y_line - 1) * textSize * charHight, padTo * textSize * charWidth, 1 * textSize * charHight, SH110X_WHITE);
+    }
+
+    display.setTextSize(textSize);
+    display.setTextColor(SH110X_WHITE, SH110X_BLACK);
+    if ((padTo > 0))
+        display.setCursor((x_char - 1) * charWidth * textSize, (y_line - 1) * charHight);
+    display.println(n);
+    display.display();
+    return 0;
+}
+
 void setup() {
     // initialize LED digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
@@ -116,6 +171,26 @@ void setup() {
     Serial.println(serial_speed);
     // Serial.println(F(""));
 
+    display.begin(i2c_Address, true);  // Address 0x3C default
+    display.cp437(true);
+    // display.setContrast (0); // dim display
+    // text display tests
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SH110X_WHITE);
+    display.setCursor(0, 0);
+    display.println("PSI: 123");
+    display.setTextSize(1);
+    display.println("BAR: 1.2");
+    // display.setTextSize(3);
+    // display.setTextColor(SH110X_BLACK, SH110X_WHITE);  // 'inverted' text
+    // display.println(3.141592);
+    // display.setTextSize(2);
+    // display.setTextColor(SH110X_WHITE);
+    // display.print("0x");
+    // display.println(0xDEADBEEF, HEX);
+    display.display();
+    delay(2000);
     // Serial.print(F("testing PSI map, 'minValue': "));
     // Serial.print(mapVoltToPSI(minValue));
     // Serial.print(F(", 'minValue-1': "));
@@ -146,7 +221,10 @@ void setup() {
     Serial.println(F("leaving setup"));
 }
 byte c = 0;
+byte loopCount = 0;
+byte lastPSI = 123;
 void loop() {
+    loopCount++;
     unsigned long currentTime = millis();  // Get the current time
     word sensorValue = analogRead(PinPSI);
     byte sensorPSI = mapVoltToPSI(sensorValue);
@@ -169,8 +247,9 @@ void loop() {
             pumpOn();
             previousTime = currentTime;  // Update the previous time
             ledBlink(LCPumpOn);
-            Serial.print(F("Pressure at turn on: "));
+            Serial.print(F("Pressure to turn on at: "));
             Serial.println(minPSI);
+
         } else {
             Serial.print(F("Not tuning on at pressure of: "));
             Serial.print(sensorPSI);
@@ -185,8 +264,15 @@ void loop() {
         ledBlink();
         if (c >= 5) {
             Serial.print(F("Pump pressure is: "));
-            Serial.println(sensorPSI);
+            Serial.print(sensorPSI);
+            Serial.print(F(", Loop count "));
+            Serial.println(loopCount);
+            // if (22 != sensorPSI) {
+            oledWriteAt(sensorPSI, 6, 1, 3, 2);
+            PSItoBar()
+            //}
             c = 0;
+            loopCount = 0;
         }
         c++;
     }
